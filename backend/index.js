07ac59a49d1b55018daf968a6bc4b22b84c110af
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-07-30 23:44:53 +0530

    Backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+app.get('/' ,(req , res) =>{
+    return res.json({message: "Hello from backend"});
+})
+
+app.listen(PORT , () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
