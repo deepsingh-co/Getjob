116cfeb1206bbbbe5940043d83965983421334dd
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-08-01 16:28:06 +0530

    feat: initialize frontend with React, Tailwind CSS, and basic routing
    - Added index.css for Tailwind CSS integration
    - Created main.jsx as the entry point for the React application
    - Implemented Auth component for user authentication interface
    - Added Home component as a placeholder for the home page
    - Configured Vite for React and Tailwind CSS support

---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,43 @@
+import react from 'react'
+import { FaRobot } from "react-icons/fa";
+import { AiFillOpenAI } from "react-icons/ai";
+import { motion } from "motion/react"
+import { FcGoogle } from "react-icons/fc";
+
+
+
+function Auth() {
+  return (
+    <div className = "w-full min-h-screen BG-[#f5f5f5] flex items-center justify-center justify-center px-6 py-20">
+        <div className = "w-full max-w-md p-8 bg-white rounded-3xl shadow-2xl border border-gray-200">
+            <div className = "flex items-center justify-center mb-6 gap-3">
+                <div className = "bg-black text-white p-2 rounded-lg">
+                    <FaRobot />
+                </div>
+        <h2 className = "font-semibold text-lg">Interview.hai</h2>
+    </div>
+    <h1 className = "text-2xl  md:text-3xl text-center leading-snug font-semibold mb-4">Continue With
+        <span className = "bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex items-center gap-2">
+            <AiFillOpenAI />
+            AI Smart Interview
+            
+        </span>
+    </h1>
+    <p className = "text-center text-sm text-gray-500 md:text-base leading-relaxed mb-8">Get your dream job with AI Smart Interview , and unlock your full potential.
+    </p>
+
+    <motion.button
+    whileHover={{ opacity:0.8,scale: 1.05 }}
+    whileTap={{ opacity:1,scale: 0.95 }} 
+    className = "w-full flex items-center justify-center gap-3 py-3 bg-black text-white rounded-full shadow-md">
+        <FcGoogle size={24} />
+        Continue with Google
+        
+    </motion.button>
+    </div>
+
+    </div>
+  )
+}
+
+export default Auth
