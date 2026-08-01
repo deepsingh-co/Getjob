90826c2274bb21f60af574459d2bd073954f0669
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-08-01 13:38:01 +0530

    Update MongoDB connection string and refactor code structure

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/connectDB.js";
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/connectDB.js"
 dotenv.config();
 
     
