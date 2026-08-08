7ea6e3e056cecbd3e692bf44178a14f57fb1d37b
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-08-08 23:23:56 +0530

    feat: implement user routes and authentication middleware for current user retrieval

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js"
+import userRouter from "./routes/user.routes.js"
 
     
 const app = express();
@@ -13,13 +14,14 @@ app.use(cors({
     credentials: true
 }));
 
-const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
+const PORT = process.env.PORT || 6000;
 app.listen(PORT , () =>{
     console.log(`Server is running on port ${PORT}`);
     connectDB();
--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,15 +1,28 @@
-
+import jwt from "jsonwebtoken"
 
 const isAuth = (req, res, next) => {
     try {
         let {token} = req.cookies
 
         if (!token) {
-            return res.status(401).json({message: "Unauthorized"})
+            return res.status(400).json({message: "Unauthorized user does not have a token"})
         }   
+
+        const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
+
+        if(!verifyToken) {
+            return res.status(400).json({message: "Unauthorized user token is not valid"})
+        }
+        req.userId = verifyToken.userId
+
+        next()
 
 
     }catch (error) {
+                return res.status(500).json({ message: `isAuth  server error ${error}` });
+
 
     }
 }
+
+export default isAuth
