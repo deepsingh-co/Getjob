9aa3221c8f8ebccbab074f5760ee2fd6cf7977fd
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-09-01 23:32:49 +0530

    fix: correct console log message for server port

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,6 @@ app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT , () =>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on the port ${PORT}`);
     connectDB();
 })
